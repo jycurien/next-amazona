356aa459085b93a9b6913cb392d5bb6b0c00a367
Jean-Yves Curien <jycurien@ime.local>
2021-09-13 21:13:34 +0200

    list of products

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,25 @@ export default function Home() {
     <Layout>
       <div>
         <h1>Products</h1>
-        <Grid container spacing={3}></Grid>
+        <Grid container spacing={3}>
+          {data.products.map(product => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia component="img" image={product.image} title={product.name}>
+                  </CardMedia>
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>  
+                </CardActionArea>
+                <CardActions>
+                  <Typography>${product.price}</Typography>  
+                  <Button size="small" color="primary">Add to cart</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </Layout>
   )
