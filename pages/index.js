911fd92b4e6793c394f6e4f0b3fe7f29649a64da
Jean-Yves Curien <jy.curien@laposte.net>
2021-09-13 20:54:23 +0200

    pics

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,22 @@
+import {
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from '@material-ui/core'
 import Layout from '../components/Layout'
+import data from '../utils/data'
 
 export default function Home() {
   return (
     <Layout>
       <div>
         <h1>Products</h1>
-        <ul>
-          <li>Product 1</li>
-          <li>Product 2</li>
-          <li>Product 3</li>
-        </ul>
+        <Grid container spacing={3}></Grid>
       </div>
     </Layout>
   )
