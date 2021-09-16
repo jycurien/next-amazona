172ecbbbf62515619bc91567a521aac917ad8e80
Jean-Yves Curien <jycurien@ime.local>
2021-09-16 17:44:21 +0200

    cart page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -16,8 +16,10 @@ import Layout from '../../components/Layout'
 import useStyles from '../../utils/styles'
 import axios from 'axios'
 import { Store } from '../../utils/Store'
+import { useRouter } from 'next/router'
 
 export default function ProductScreen(props) {
+  const router = useRouter()
   const { dispatch } = useContext(Store)
   const { product } = props
   const classes = useStyles()
@@ -32,6 +34,7 @@ export default function ProductScreen(props) {
       return
     }
     dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity: 1 } })
+    router.push('/cart')
   }
 
   return (
