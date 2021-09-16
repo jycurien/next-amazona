375933ae470e9e928c3a743c3951a6a3a1a741cb
Jean-Yves Curien <jycurien@ime.local>
2021-09-16 11:24:09 +0200

    add product to cart

--- components/Layout.js
+++ components/Layout.js
@@ -10,15 +10,16 @@ import {
   createTheme,
   ThemeProvider,
   CssBaseline,
-  Switch
-} from '@material-ui/core';
+  Switch,
+  Badge,
+} from '@material-ui/core'
 import useStyles from '../utils/styles'
-import { Store } from '../utils/Store';
-import Cookies from 'js-cookie';
+import { Store } from '../utils/Store'
+import Cookies from 'js-cookie'
 
 export default function Layout({ title, description, children }) {
-  const { state, dispatch } = useContext(Store);
-  const { darkMode } = state;
+  const { state, dispatch } = useContext(Store)
+  const { darkMode, cart } = state
   const theme = createTheme({
     typography: {
       h1: {
@@ -41,30 +42,30 @@ export default function Layout({ title, description, children }) {
         main: '#208080',
       },
     },
-  });
+  })
 
   const classes = useStyles()
   const darkModeChangeHandler = () => {
-    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
-    const newDarkMode = !darkMode;
-    Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF');
-  };
+    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' })
+    const newDarkMode = !darkMode
+    Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF')
+  }
 
   return (
     <div>
       <Head>
         <title>{title ? `${title} - Next Amazona` : 'Next Amazona'}</title>
-        {description && <meta name="description" content={description}></meta>}
+        {description && <meta name='description' content={description}></meta>}
         <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
+          rel='stylesheet'
+          href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+        />
       </Head>
-       <ThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
-        <AppBar position="static" className={classes.navbar}>
+        <AppBar position='static' className={classes.navbar}>
           <Toolbar>
-            <NextLink href="/" passHref>
+            <NextLink href='/' passHref>
               <Link>
                 <Typography className={classes.brand}>amazona</Typography>
               </Link>
@@ -75,10 +76,21 @@ export default function Layout({ title, description, children }) {
                 checked={darkMode}
                 onChange={darkModeChangeHandler}
               ></Switch>
-              <NextLink href="/cart" passHref>
-                <Link>Cart</Link>
+              <NextLink href='/cart' passHref>
+                <Link>
+                  {cart.cartItems.length > 0 ? (
+                    <Badge
+                      color='secondary'
+                      badgeContent={cart.cartItems.length}
+                    >
+                      Cart
+                    </Badge>
+                  ) : (
+                    'Cart'
+                  )}
+                </Link>
               </NextLink>
-              <NextLink href="/login" passHref>
+              <NextLink href='/login' passHref>
                 <Link>Login</Link>
               </NextLink>
             </div>
